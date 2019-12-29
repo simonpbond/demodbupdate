234d284b92de829e518bb0ee6d4f9a144a0f0c14
Simon B <simonpbond@users.noreply.github.com>
2019-12-29 18:07:08 +0545

    first commit

---
+++ DemoDbUpdate/Client/Interop/InteropMethods.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDbUpdate.Client.Interop
+{
+    public class InteropMethods
+    {
+        public static Action OnDataChanged;
+
+        [JSInvokable]
+        public static void DataChanged()
+        {
+            Console.WriteLine("Successfully received notification that data has changed from Server SignalR Hub");
+            OnDataChanged.Invoke();
+        }
+
+    }
+}
---
+++ DemoDbUpdate/Server/Hubs/NotificationHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDbUpdate.Server.Hubs
+{
+    public class NotificationHub : Hub
+    {
+
+        public override async Task OnConnectedAsync()
+        {
+            Debug.Write("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Server: DataTime: {0} - Signalr client has connected.", DateTime.Now.ToString());
+        }
+
+    }
+}
