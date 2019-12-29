b4bd8b8181f339383af82dca8efec5e5a07fb341
Simon B <simonpbond@users.noreply.github.com>
2019-12-29 19:14:45 +0545

    update

---
+++ DemoDbUpdate/Server/MockDatabase.cs
@@ -0,0 +1,28 @@
+﻿using DemoDbUpdate.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDbUpdate.Server
+{
+    public class MockDatabase
+    {
+
+
+        public static List<WeatherForecast> mockDatabaseWeatherForecast = new List<WeatherForecast>();
+
+
+        public static void SaveToDatabase(IEnumerable<WeatherForecast> weatherForecasts)
+        {
+            mockDatabaseWeatherForecast.AddRange(weatherForecasts);
+        }
+
+        public static IEnumerable<WeatherForecast> GetFromDatabase()
+        {
+            return mockDatabaseWeatherForecast;
+        }
+
+
+    }
+}
